788403318def54535354bedc6ef83543f79c5b9f
Amine A <amine-98@live.fr>
2020-06-19 19:48:36 +0200

    Added $SUBFOLDER argument to place data in this folder

--- BatchExtractSubtitles.sh
+++ BatchExtractSubtitles.sh
@@ -1,49 +1,57 @@
 #!/bin/bash
-set -e 
+set -e
 
 extractSubsAndAttachments() {
-    file="${1}";basename="${1%.*}"
+    file="${1}"
+    basename="${1%.*}"
     : ${SUBEXT:="ass"} #Sets the default value if not specified
+    originaldir=pwd
 
-    mkdir -p "$basename/"
-    cd "$basename/"
+    if [[ ! -z $SUBFOLDER ]]; then
+        mkdir -p $SUBFOLDER
+        cd $SUBFOLDER
+    fi
 
-    subsmappings=$(ffprobe -loglevel error -select_streams s -show_entries stream=index:stream_tags=language,title -of csv=p=0 "../${file}")
+    mkdir -p "$basename"
+    cd "$basename"
+
+    subsmappings=$(ffprobe -loglevel error -select_streams s -show_entries stream=index:stream_tags=language,title -of csv=p=0 "${originaldir}/${file}")
     #Results formatted as : 2,eng,title
-    
+
     #IFS is the command delimiter - https://bash.cyberciti.biz/guide/$IFS
     #We back it up before changing it to a ',' as used in the mappings
-    OLDIFS=$IFS;IFS=,
+    OLDIFS=$IFS
+    IFS=,
 
     (while read idx lang title; do
 
         if [ -z "$lang" ]; then
-            lang="und" 
+            lang="und"
             #When the subtitle language isn't present in the file, we note it as undefined and extract it regardless of the parameters
         else
             if [[ ! -z "$LANGS" ]] && [[ "$LANGS" != *$lang* ]]; then
-            #If subtitles language restrictions were provided, we check that the subtitles lang is one of them before proceeding 
-            echo "Skipping ${lang} subtitle #${idx}"
-            continue
+                #If subtitles language restrictions were provided, we check that the subtitles lang is one of them before proceeding
+                echo "Skipping ${lang} subtitle #${idx}"
+                continue
             fi
         fi
 
         echo "Extracting ${lang} subtitle #${idx} named '$title' to .${SUBEXT}, from ${file}"
         formattedTitle="${title//[^[:alnum:] -]/}" #We format the track title to avoid issues using it in a filename.
         ffmpeg -y -nostdin -hide_banner -loglevel error -i \
-        "../${file}" -map 0:"$idx" "${formattedTitle}_${idx}_${lang}_$basename.$SUBEXT"
+            "${originaldir}/${file}" -map 0:"$idx" "${formattedTitle}_${idx}_${lang}_$basename.$SUBEXT"
         # The -y option replaces existing files.
 
     done <<<"${subsmappings}")
 
     echo "Dumping attachments from $file"
-    ffmpeg -nostdin -hide_banner -loglevel quiet -dump_attachment:t "" -i "../${file}" || true #"One or more attachments could not be extracted."
-    # Despite successful extraction, the error "At least one output file must be specified" seems to always appear. 
+    ffmpeg -nostdin -hide_banner -loglevel quiet -dump_attachment:t "" -i "${originaldir}/${file}" || true #"One or more attachments could not be extracted."
+    # Despite successful extraction, the error "At least one output file must be specified" seems to always appear.
     # The "|| true" part allows us to continue the script regardless.
 
     #Restore previous values
     IFS=$OLDIFS
-    cd ..
+    cd $originaldir
 }
 for f in *.mkv; do
     extractSubsAndAttachments "$f"
